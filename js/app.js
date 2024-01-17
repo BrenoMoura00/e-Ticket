d5a79d9d5d078928494b22af73106d5830d1a2a5
BrenoMoura00 <brenoluisadv@gmail.com>
2024-01-16 22:20:55 -0300

    aplicando a lógica

--- js/app.js
+++ js/app.js
@@ -4,5 +4,14 @@ function comprar(){
     let quantidadeInput = parseInt(document.getElementById('qtd').value);
     let eletmentoQuantidade = document.getElementById(`qtd-${tipo}`);
     let quantidadeDisponivel = parseInt(eletmentoQuantidade.textContent);
-    
+
+    //aplicando a lógica
+ if(quantidadeDisponivel >= quantidadeInput){
+    eletmentoQuantidade.textContent = quantidadeDisponivel - quantidadeInput;
+    alert('Compra realizada com sucesso')
+} else {
+    alert('Verifique a quantidade de cadeiras dísponiveis.')
+}
+
+
 }
